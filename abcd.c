144009ceed9b851d9e15b515632804f4bf192d51
YuanPan <yuanpan666@163.com>
2022-06-04 18:51:43 +0800

    Add files via upload

---
+++ abcd.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define THRUM  4
+
+static pthread_mutex_t mut[THRUM];
+
+static int next(int n)
+{
+  if( (n+1)  == THRUM)
+  {
+    return 0;
+  }
+  else
+  {
+    return n+1;
+  }
+}
+
+static void *thr_func(void *p)
+{
+
+  int n = (int)p;
+  int c ='a' + (int)p;
+  while (1)
+  {
+    pthread_mutex_lock(mut+n);
+    write(1,&c,1);
+    
+    pthread_mutex_unlock(mut+next(n));
+
+    //pthread_mutex_lock(mut+n);
+  }
+
+  pthread_exit(NULL);
+  
+
+}
+
+int main()
+{
+  pthread_t tid[THRUM];
+  int err;
+  char p[4] ="tttt";
+  for (int  i = 0; i < THRUM; i++)
+  {
+    /* code */
+
+    pthread_mutex_init(mut+i,NULL);
+    pthread_mutex_lock(mut + i);
+   // write(1,p,4);
+    err = pthread_create(tid+i,NULL,thr_func,(void *)i);
+   
+    if(err)
+    {
+      fprintf(stderr,"pthread_create():%s\n",strerror(err));
+      exit(1);
+
+    }
+  }
+
+  pthread_mutex_unlock(mut + 0);
+
+  alarm(1);
+  for(int i=0; i < THRUM;i++)
+  {
+    pthread_join(tid[i],NULL);
+  }
+  
+
+  exit(0);
+}
